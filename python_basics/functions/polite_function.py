02929c26128edf69bf70e878a5a06b6007c7a111
dlobe <lobedaf@yahoo.com>
2019-07-15 12:16:07 -0400

    Initial Commit

---
+++ python_basics/functions/polite_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 15 08:38:03 2019
+
+@author: davidlobe
+"""
+#polite_function
+def make_polite(sentence):
+    polite_sentence = sentence + ' please'
+    return polite_sentence
+print(make_polite('Pass the salt'))
